d1e25b8179a13b2ff69840530898be6fa465f117
Oscar <oscarcarlstrom@christismacbook.home>
2014-10-22 16:26:34 +0200

    map tab update

--- apps/ColorChange/common/js/Meet.js
+++ apps/ColorChange/common/js/Meet.js
@@ -4,9 +4,23 @@ currentPage.init = function() {
 	WL.Logger.debug("Meet :: init");
 };
 
-var visibleExtension = "";
+currentPage.add = function() {
+	//add meetin
+	WL.Logger.debug("addMeetIn :: pressed");
+	pageHistory.push(path + "pages/Meet.html");
+	$("#pagePort").load(path + "pages/SelectFriends.html");
+};
+
+var extensions = [];
+for(var i=0;extensions.length;i++) {
+	extensions[i] = false;
+}
+$(".attendBtn").css("background-image", "url(images/Gray-radio.png)");
+$(".maybeBtn").css("background-image", "url(images/Gray-radio.png)");
+$(".notAttendBtn").css("background-image", "url(images/Gray-radio.png)");
 
 //Read jSON data from web
+/*
 $.getJSON('http://meetin.mybluemix.net/meetins', function(data) {
 	for(var i=0;i<data.length;i++) {
 		var n = i+1;
@@ -17,7 +31,7 @@ $.getJSON('http://meetin.mybluemix.net/meetins', function(data) {
 		var time = meeting + "Time";
 		
 		$(req).text(data[i].requester);
-		var tid = data[i].starttime + " - " + data[i].endtime
+		var tid = data[i].starttime + " - " + data[i].endtime;
 		$(time).text(tid);
 		$(loc).text(data[i].beaconid);
 		
@@ -30,81 +44,85 @@ $.getJSON('http://meetin.mybluemix.net/meetins', function(data) {
 		$(picture).css("background-repeat", "no-repeat");
 		$(picture).css("background-size", "contain");
 		$(meeting).css("visibility", "visible");
-		
-		/*if(n == data.length) {
-			$(meeting).css("border-bottom-style", "none");
-			$(meeting).css("border-bottom-color", "none");
-			$(meeting).css("border-bottom-width", "none");
-		}*/
 	}
-});
+});*/
 
-$(".roomPicker").append("<option value=Ada>Ada</option>");
-$(".roomPicker").append("<option value=C>C</option>");
-$(".roomPicker").append("<option value=Euler>Euler</option>");
-
-$(".timePicker").append("<option value=00:00>00:00</option>");
-$(".timePicker").append("<option value=01:00>01:00</option>");
-$(".timePicker").append("<option value=02:00>02:00</option>");
-$(".timePicker").append("<option value=03:00>03:00</option>");
-$(".timePicker").append("<option value=04:00>04:00</option>");
-$(".timePicker").append("<option value=05:00>05:00</option>");
-$(".timePicker").append("<option value=06:00>06:00</option>");
-$(".timePicker").append("<option value=07:00>07:00</option>");
-$(".timePicker").append("<option value=08:00>08:00</option>");
-$(".timePicker").append("<option value=09:00>09:00</option>");
-$(".timePicker").append("<option value=10:00>10:00</option>");
-$(".timePicker").append("<option value=11:00>11:00</option>");
-$(".timePicker").append("<option value=12:00>12:00</option>");
-$(".timePicker").append("<option value=13:00>13:00</option>");
-$(".timePicker").append("<option value=14:00>14:00</option>");
-$(".timePicker").append("<option value=15:00>15:00</option>");
-$(".timePicker").append("<option value=16:00>16:00</option>");
-$(".timePicker").append("<option value=17:00>17:00</option>");
-$(".timePicker").append("<option value=18:00>18:00</option>");
-$(".timePicker").append("<option value=19:00>19:00</option>");
-$(".timePicker").append("<option value=20:00>20:00</option>");
-$(".timePicker").append("<option value=21:00>21:00</option>");
-$(".timePicker").append("<option value=22:00>22:00</option>");
-$(".timePicker").append("<option value=23:00>23:00</option>");
-
-
-function editMeeting(n) {
-	var extension = "#meetExtension"+n;
-	
-	if(visibleExtension == "") {
-		$(extension).css("height", "110px");
-		$(extension).css("line-height", "20px");
-		$(extension).css("display", "block");
-		visibleExtension = extension;
-	}
-	else if(visibleExtension == extension) {
-		$(visibleExtension).css("display", "none");
-		$(visibleExtension).css("height", "0");
-		$(extension).css("line-height", "0");
-		visibleExtension = "";
+function expandBtnClick(n) {
+	if(extensions[n-1]) {
+		hide(n);
+		extensions[n-1] = false;
 	}
 	else {
-		$(visibleExtension).css("display", "none");
-		$(visibleExtension).css("height", "0px");
-		$(extension).css("height", "120px");
-		$(extension).css("line-height", "20px");
-		$(extension).css("display", "block");
-		visibleExtension = extension;
+		expand(n);
+		extensions[n-1] = true;
 	}
 }
 
-currentPage.add = function() {
-	//add meetin
-	WL.Logger.debug("addMeetIn :: pressed");
-	pageHistory.push(path + "pages/Meet.html");
-	$("#pagePort").load(path + "pages/SelectFriends.html");
-};
+function hide(n) {
+	var meetIn = "#meetIn"+n;
+	var attendBtn = "#attendBtn"+n;
+	var maybeBtn = "#maybeBtn"+n;
+	var notAttendBtn = "#notAttendBtn"+n;
+	var replyMenu = "#reply-menu"+n;
+	var arrow = "#expandArrow"+n;
+	
+	$(attendBtn).css("-webkit-transition", "0ms");
+	$(attendBtn).css("visibility", "hidden");
+	
+	$(maybeBtn).css("visibility", "hidden");
+	$(maybeBtn).css("-webkit-transition", "0ms");
+	
+	$(notAttendBtn).css("visibility", "hidden");
+	$(notAttendBtn).css("-webkit-transition", "0ms");
+	
+	$(meetIn).css("height", "60");
+	$(replyMenu).css("-webkit-transition", "400ms");
+	
+	$(replyMenu).css("visibility", "hidden");
+	$(arrow).css("-webkit-transform", "rotate(-0deg)");
+}
 
-function changeRoom(n) {
+function expand(n) {
+	var meetIn = "#meetIn"+n;
+	var attendBtn = "#attendBtn"+n;
+	var maybeBtn = "#maybeBtn"+n;
+	var notAttendBtn = "#notAttendBtn"+n;
+	var replyMenu = "#reply-menu"+n;
+	var arrow = "#expandArrow"+n;
+	
+	$(meetIn).css("height", "300");
+	$(replyMenu).css("visibility", "visible");
+	$(arrow).css("-webkit-transform", "rotate(180deg)");
+
+	$(attendBtn).css("-webkit-transition", "400ms");
+	$(attendBtn).css("visibility", "visible");
+	
+	$(maybeBtn).css("-webkit-transition", "800ms");
+	$(maybeBtn).css("visibility", "visible");
+	
+	$(notAttendBtn).css("-webkit-transition", "1200ms");
+	$(notAttendBtn).css("visibility", "visible");
+}
+
+function deleteMeetIn(n) {
 	
 }
 
-function changeTime(n) {
-	
+
+function setAttending(n) {
+	$(".maybeBtn").css("background-image", "url(images/Gray-radio.png)");
+	$(".notAttendBtn").css("background-image", "url(images/Gray-radio.png)");
+	$(".attendBtn").css("background-image", "url(images/Gray-radio-selected.png)");
+}
+
+function setMaybe(n) {
+	$(".attendBtn").css("background-image", "url(images/Gray-radio.png)");
+	$(".notAttendBtn").css("background-image", "url(images/Gray-radio.png)");
+	$(".maybeBtn").css("background-image", "url(images/Gray-radio-selected.png)");
+}
+
+function setNotAttending(n) {
+	$(".attendBtn").css("background-image", "url(images/Gray-radio.png)");
+	$(".maybeBtn").css("background-image", "url(images/Gray-radio.png)");
+	$(".notAttendBtn").css("background-image", "url(images/Gray-radio-selected.png)");
 }
